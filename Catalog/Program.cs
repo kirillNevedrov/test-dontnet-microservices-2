6b21ceb7406175324bffbfd05b2f1b6081e8d7b0
Kirill Nevedrov <kirill.nevedrov@gmail.com>
2023-05-15 23:28:16 +0200

    add catalog api

--- Catalog/Program.cs
+++ Catalog/Program.cs
@@ -6,6 +6,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<CatalogContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration["ConnectionString"],
+                                        sqlServerOptionsAction: sqlOptions =>
+                                        {
+                                            sqlOptions.MigrationsAssembly(typeof(Program).GetTypeInfo().Assembly.GetName().Name);
+                                            //Configuring Connection Resiliency: https://docs.microsoft.com/en-us/ef/core/miscellaneous/connection-resiliency 
+                                            sqlOptions.EnableRetryOnFailure(maxRetryCount: 15, maxRetryDelay: TimeSpan.FromSeconds(30), errorNumbersToAdd: null);
+                                        });
+            });
 
 var app = builder.Build();
 
